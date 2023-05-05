10449fd0e8b7ae258851e680f494a3eee2f2f690
Elsa <edelmas@sempo10009.glm.lan>
2023-05-05 17:07:37 +0200

    ~/components/Icon - add toggle icon

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -5,12 +5,16 @@ import ArrowLeft from './assets/arrow-left.svg'
 import ArrowRight from './assets/arrow-right.svg'
 import FullscreenClose from './assets/fullscreen-close.svg'
 import FullscreenOpen from './assets/fullscreen-open.svg'
+import ToggleClose from './assets/toggle-close.svg'
+import ToggleOpen from './assets/toggle-open.svg'
 
 enum Icons {
   ARROW_LEFT,
   ARROW_RIGHT,
   FULLSCREEN_CLOSE,
-  FULLSCREEN_OPEN
+  FULLSCREEN_OPEN,
+  TOGGLE_CLOSE,
+  TOGGLE_OPEN
 }
 
 const iconsNamesToUrlMap = new Map<Icons, string>()
@@ -18,6 +22,8 @@ iconsNamesToUrlMap.set(Icons.ARROW_LEFT, ArrowLeft)
 iconsNamesToUrlMap.set(Icons.ARROW_RIGHT, ArrowRight)
 iconsNamesToUrlMap.set(Icons.FULLSCREEN_CLOSE, FullscreenClose)
 iconsNamesToUrlMap.set(Icons.FULLSCREEN_OPEN, FullscreenOpen)
+iconsNamesToUrlMap.set(Icons.TOGGLE_CLOSE, ToggleClose)
+iconsNamesToUrlMap.set(Icons.TOGGLE_OPEN, ToggleOpen)
 
 interface Props {
   file: Icons
