42f59514c64f4cb21d625fd1efe1c1cf990ead0f
Elsa <edelmas@sempo10009.glm.lan>
2023-05-12 17:21:35 +0200

    add ~/components/ArticleUI

--- src/apps/anycomp/index.tsx
+++ src/apps/anycomp/index.tsx
@@ -3,16 +3,19 @@ import { Options, Renderer } from 'shared/utils/lm-page-apps'
 
 import flattenGetters from '~/utils/flatten-getters'
 
-import ImageWithTextOverlay from '~/components/ImageWithTextOverlay'
 import ArticleThumb from '~/components/ArticleThumb'
+import ArticleUI from '~/components/ArticleUI'
+import ImageWithTextOverlay from '~/components/ImageWithTextOverlay'
 
 export enum ComponentsLib {
   ARTICLE_THUMB = 'ArticleThumb',
+  ARTICLE_UI = 'ArticleUI',
   IMAGE_WITH_TEXT_OVERLAY = 'ImageWithTextOverlay',
 }
 
 export const componentsMap = new Map<ComponentsLib, any>()
 componentsMap.set(ComponentsLib.ARTICLE_THUMB, ArticleThumb)
+componentsMap.set(ComponentsLib.ARTICLE_UI, ArticleUI)
 componentsMap.set(ComponentsLib.IMAGE_WITH_TEXT_OVERLAY, ImageWithTextOverlay)
 
 /* * * * * * * * * * * * * * * * * * *
