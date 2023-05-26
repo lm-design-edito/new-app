ce4a0950fdf807d1f3f52b755533491854628427
Maxime Fabas <fabas@lemonde.fr>
2023-05-26 22:33:31 +0200

    ~/utils/selector-to-element - minor formal updates

--- src/utils/selector-to-element/index.ts
+++ src/utils/selector-to-element/index.ts
@@ -1,29 +1,29 @@
-import { toString } from "../cast";
+import { toString } from '../cast'
 
 export default function selectorToElement (selector: string) {
   const selectorTag = selector.match(/^.*?(?=[#|\.|\/[]|$)/)
   const tag = selectorTag && selectorTag[0] ? selectorTag[0] : 'div'
   const element = document.createElement(tag)
-
-  selector.split(/(?=[#|.[])/).forEach((attribute) => {
+  selector.split(/(?=[#|.[])/).forEach(_attribute => {
     try {
-      const _attribute = attribute.trim();
-      if (_attribute.includes('.')) {
-        element.classList.add(_attribute.replace('.', ''))
-      } else if (_attribute.includes('#')) {
-        element.setAttribute('id', _attribute.replace('#', ''));
-      } else if (_attribute.includes('[')) {
-        const keyValueAttribute = _attribute.match(/(?!\[).+(?<!\])/);
+      const attribute = _attribute.trim()
+      const isClassAttr = attribute.includes('.')
+      const isIdAttr = attribute.includes('#')
+      const isOtherAttr = attribute.includes('[')
+      if (isClassAttr) element.classList.add(attribute.replace('.', ''))
+      else if (isIdAttr) element.setAttribute('id', attribute.replace('#', ''))
+      else if (isOtherAttr) {
+        const keyValueAttribute = attribute.match(/(?!\[).+(?<!\])/)
         if (keyValueAttribute && keyValueAttribute.length) {
-          const [key, value = ''] = keyValueAttribute[0].replace(/["']/gm, '').split('=');
-          if (key) {
-            element.setAttribute(toString(key), toString(value));
-          }
+          const [key, value = ''] = keyValueAttribute[0]
+            .replace(/["']/gm, '')
+            .split('=')
+          if (key) element.setAttribute(toString(key), toString(value))
         }
       }
     } catch(e) {
       console.log('ERR selectorToElement', e)
     }
-  });
-  return element;
+  })
+  return element
 }
