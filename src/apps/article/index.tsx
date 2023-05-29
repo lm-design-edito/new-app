04b7a8164bd46215f615873e131fc3d42f413c54
Maxime Fabas <fabas@lemonde.fr>
2023-05-29 17:52:33 +0200

    ~/apps/article - use toVnode now

--- src/apps/article/index.tsx
+++ src/apps/article/index.tsx
@@ -2,8 +2,9 @@ import { isValidElement, render } from 'preact'
 import Article, { Props as ArticleProps, ArticleElementProps } from '~/components/Article'
 import { ElementType as BasicElementType } from '~/components/Article/BasicTextElement'
 import { Options, Renderer } from '~/shared/lm-page-apps'
-import { toBoolean } from '~/utils/cast'
+import { toBoolean, toVNode } from '~/utils/cast'
 import nodesToVNodes from '~/utils/nodes-to-vnodes'
+import selectorToElement from '~/utils/selector-to-element'
 
 /* * * * * * * * * * * * * * * * * * *
  * RENDERER
@@ -68,10 +69,8 @@ function unknownToElement (unknown: unknown): ArticleElementProps|undefined {
   const typeIsElementType = basicElementTypes.includes(type as BasicElementType)
   if (type === undefined || typeIsElementType) {
     const elementProps: ArticleElementProps = { type: type as BasicElementType }
+    if (content !== undefined) { elementProps.content = toVNode(content) }
     if (typeof customClass === 'string') elementProps.customClass = customClass
-    if (typeof content === 'string') elementProps.content = content
-    else if (content instanceof HTMLElement) elementProps.content = nodesToVNodes(content)[0]
-    else if (isValidElement(content)) elementProps.content = content
     if (typeof url === 'string') elementProps.url = url
     return elementProps
   }
@@ -80,12 +79,8 @@ function unknownToElement (unknown: unknown): ArticleElementProps|undefined {
     if (typeof customClass === 'string') elementProps.customClass = customClass
     if (typeof url === 'string') elementProps.url = url
     if (typeof alt === 'string') elementProps.alt = alt
-    if (typeof credits === 'string') elementProps.credits = credits
-    else if (credits instanceof HTMLElement) elementProps.credits = nodesToVNodes(credits)[0]
-    else if (isValidElement(credits)) elementProps.credits = credits
-    if (typeof description === 'string') elementProps.description = description
-    else if (description instanceof HTMLElement) elementProps.description = nodesToVNodes(description)[0]
-    else if (isValidElement(description)) elementProps.description = description
+    if (credits !== undefined) { elementProps.credits = toVNode(credits) }
+    if (description !== undefined) { elementProps.description = toVNode(description) }
     if (captionPosition === 'overlay') elementProps.captionPosition = captionPosition
     else elementProps.captionPosition = 'below'
     return elementProps
@@ -95,21 +90,15 @@ function unknownToElement (unknown: unknown): ArticleElementProps|undefined {
     if (typeof customClass === 'string') elementProps.customClass = customClass
     elementProps.subscribed = toBoolean(subscribed)
     if (typeof url === 'string') elementProps.url = url
-    if (typeof label === 'string') elementProps.label = label
-    else if (label instanceof HTMLElement) elementProps.label = nodesToVNodes(label)[0]
-    else if (isValidElement(label)) elementProps.label = label
-    if (typeof content === 'string') elementProps.content = content
-    else if (content instanceof HTMLElement) elementProps.content = nodesToVNodes(content)[0]
-    else if (isValidElement(content)) elementProps.content = content
+    if (label !== undefined) { elementProps.label = toVNode(label) }
+    if (content !== undefined) { elementProps.content = toVNode(content) }
     return elementProps
   }
   if (type === 'read-in-english') {
     const elementProps: ArticleElementProps = { type }
     if (typeof customClass === 'string') elementProps.customClass = customClass
     if (typeof url === 'string') elementProps.url = url
-    if (typeof content === 'string') elementProps.content = content
-    else if (content instanceof HTMLElement) elementProps.content = nodesToVNodes(content)[0]
-    else if (isValidElement(content)) elementProps.content = content
+    if (content !== undefined) { elementProps.content = toVNode(content) }
     return elementProps
   }
   if (type === 'quote') { /* [WIP] */}
@@ -117,9 +106,7 @@ function unknownToElement (unknown: unknown): ArticleElementProps|undefined {
   if (type === 'html') {
     const elementProps: ArticleElementProps = { type }
     if (typeof customClass === 'string') elementProps.customClass = customClass
-    if (typeof content === 'string') elementProps.content = content
-    else if (content instanceof HTMLElement) elementProps.content = nodesToVNodes(content)[0]
-    else if (isValidElement(content)) elementProps.content = content
+    if (content !== undefined) { elementProps.content = toVNode(content) }
     return elementProps
   }
 }
