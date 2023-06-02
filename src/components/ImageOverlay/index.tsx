24ac28f93f48e82ffbfdd7cf908352210ba9dac1
Elsa <edelmas@sempo10009.glm.lan>
2023-06-02 16:25:07 +0200

    ~/components/ImageOverlay - add toggle caption option

--- src/components/ImageOverlay/index.tsx
+++ src/components/ImageOverlay/index.tsx
@@ -1,7 +1,15 @@
 import { Component, JSX, VNode } from 'preact'
+
+import ToggleButton from '~/components/ToggleButton'
+import Drawer from '~/components/Drawer'
 import Img from '~/components/Img'
+
 import bem from '~/utils/bem'
 import styles from './styles.module.scss'
+
+type State = {
+  captionIsOpen: boolean
+}
 
 export type Props = {
   imageUrl?: string
@@ -12,22 +20,42 @@ export type Props = {
   textBottom?: string | VNode
   shadeLinearGradient?: string
   shadeBlendMode?: string
+  toggleCaptionBtn?: boolean
+  openCaptionText?: string
+  closeCaptionText?: string
+  openCaptionIcon?: VNode
+  closeCaptionIcon?: VNode
 }
 
-export default class ImageOverlay extends Component<Props, {}> {
+export default class ImageOverlay extends Component<Props, State> {
   bemClss = bem('lm-image-overlay')
+
+  state = {
+    captionIsOpen: false,
+  }
+
+  /* * * * * * * * * * * * * * * * * * *
+     * CONSTRUCTOR
+     * * * * * * * * * * * * * * * * * * */
+  constructor(props: Props) {
+    super(props)
+
+    this.toggleCaption = this.toggleCaption.bind(this)
+  }
+
+  toggleCaption() {
+    this.setState(curr => ({
+      ...curr,
+      captionIsOpen: !curr.captionIsOpen
+    }))
+  }
 
   /* * * * * * * * * * * * * * * * * * *
    * RENDER
    * * * * * * * * * * * * * * * * * * */
-  render () {
-    const { props, bemClss } = this
-    const wrapperClasses = [bemClss.value, styles['wrapper']]
-    const imageClasses = [bemClss.elt('image').value, styles['image']]
-    const shadeClasses = [bemClss.elt('shade').value, styles['shade']]
-    const textTopClasses = [bemClss.elt('text-top').value, styles['text-top']]
-    const textMiddleClasses = [bemClss.elt('text-middle').value, styles['text-middle']]
-    const textBottomClasses = [bemClss.elt('text-bottom').value, styles['text-bottom']]
+  render() {
+    const { props, state, bemClss } = this
+
     const {
       imageUrl,
       imageAlt,
@@ -36,14 +64,31 @@ export default class ImageOverlay extends Component<Props, {}> {
       textMiddle,
       textBottom,
       shadeLinearGradient,
-      shadeBlendMode
+      shadeBlendMode,
+      toggleCaptionBtn,
+      closeCaptionText,
+      openCaptionText,
+      closeCaptionIcon,
+      openCaptionIcon
     } = props
+
+    const wrapperClasses = [bemClss.mod({
+      'caption-open': toggleCaptionBtn === true && state.captionIsOpen === true,
+      'caption-closed': toggleCaptionBtn === true && state.captionIsOpen === false
+    }).value, styles['wrapper']]
+    const imageClasses = [bemClss.elt('image').value, styles['image']]
+    const shadeClasses = [bemClss.elt('shade').value, styles['shade']]
+    const textTopClasses = [bemClss.elt('text-top').value, styles['text-top']]
+    const textMiddleClasses = [bemClss.elt('text-middle').value, styles['text-middle']]
+    const textBottomClasses = [bemClss.elt('text-bottom').value, styles['text-bottom']]
+
     const wrapperStyle = {
       ['--shade-gradient']: shadeLinearGradient !== undefined
         ? `linear-gradient(${shadeLinearGradient})`
         : undefined,
       ['--shade-blend-mode']: shadeBlendMode
     }
+
     return <div
       className={wrapperClasses.join(' ')}
       style={wrapperStyle}>
@@ -57,9 +102,29 @@ export default class ImageOverlay extends Component<Props, {}> {
       {/* shade */}
       {<div className={shadeClasses.join(' ')} />}
       {/* text */}
-      {textTop && <div className={textTopClasses.join(' ')}>{textTop}</div>}
-      {textMiddle && <div className={textMiddleClasses.join(' ')}>{textMiddle}</div>}
-      {textBottom && <div className={textBottomClasses.join(' ')}>{textBottom}</div>}
+      {[
+        { content: textTop, classes: textTopClasses },
+        { content: textMiddle, classes: textMiddleClasses },
+        { content: textBottom, classes: textBottomClasses }
+      ].map((text) => {
+        if (text.content === undefined) return
+        if (toggleCaptionBtn !== true) return <div className={text.classes.join(' ')}>{text.content}</div>
+        else return (
+          <div className={text.classes.join(' ')}>
+            <ToggleButton
+              isOpen={state.captionIsOpen}
+              openText={openCaptionText}
+              closeText={closeCaptionText}
+              openIcon={openCaptionIcon}
+              closeIcon={closeCaptionIcon}
+              onClick={this.toggleCaption}
+            />
+            <Drawer opened={state.captionIsOpen}>
+              {text.content}
+            </Drawer>
+          </div>
+        )
+      })}
     </div>
   }
 }
