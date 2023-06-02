d9172c303d055445e1e971c5a2115cbe3f67ce7c
Elsa <edelmas@sempo10009.glm.lan>
2023-06-02 16:05:08 +0200

    add ~/components/ToggleButton

---
+++ src/components/ToggleButton/index.tsx
@@ -0,0 +1,36 @@
+import { Component, JSX, VNode } from 'preact'
+
+import bem from '~/utils/bem'
+import styles from './styles.module.scss'
+
+export type Props = {
+  isOpen?: boolean
+  openText?: string
+  closeText?: string
+  openIcon?: VNode
+  closeIcon?: VNode
+  onClick?: () => void
+}
+
+export default class ToggleButton extends Component<Props, {}> {
+  bemClss = bem('lm-toggle-button')
+
+  /* * * * * * * * * * * * * * * * * * *
+   * RENDER
+   * * * * * * * * * * * * * * * * * * */
+  render() {
+    const { props, bemClss } = this
+
+    const wrapperClasses = [bemClss.mod({ 'is-open': props.isOpen }).value, styles['wrapper']]
+
+    const text = props.isOpen ? props.closeText : props.openText
+    const icon = props.isOpen ? props.closeIcon : props.openIcon
+
+    return (
+      <div className={wrapperClasses.join(' ')} onClick={props.onClick}>
+        {text !== undefined && <span>{text}</span>}
+        {icon !== undefined && icon}
+      </div>
+    )
+  }
+}
