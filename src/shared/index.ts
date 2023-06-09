c059249f975d407f7a7dd39977f8b9af71ee2dcf
Maxime Fabas <fabas@lemonde.fr>
2023-06-09 15:16:40 +0200

    ~/shared/index - find another way to ensure project level stylesheets placement after more generic stylesheets

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -29,19 +29,23 @@ export async function initPage () {
   silentLogger.log('page-init', `Start init the page from ${SCRIPTS_INDEX_URL.toString()}`)
 
   // Keep lm-page-stylesheet elements at the end of the head
-  const headNode = document.head
-  let headNodeLastMutatedStylesheetNodes: Node[] = []
-  const headObserver = new MutationObserver(mutationList => {
-    const onlyStylesheetsMutated = mutationList.every(({ addedNodes, removedNodes }) => {
-      const nodes = [...addedNodes, ...removedNodes]
-      return nodes.every(node => headNodeLastMutatedStylesheetNodes.includes(node))
-    })
-    if (onlyStylesheetsMutated) return;
-    const lmPageStylesheets = [...document.querySelectorAll('.lm-page-stylesheet')]
-    headNodeLastMutatedStylesheetNodes = [...lmPageStylesheets]
-    lmPageStylesheets.forEach(stylesheet => headNode.appendChild(stylesheet))
-  })
-  headObserver.observe(headNode, { childList: true })
+  // const headNode = document.head
+  // let headNodeLastMutatedStylesheetNodes: Node[] = []
+  // const headObserver = new MutationObserver(mutationList => {
+  //   const onlyStylesheetsMutated = mutationList.every(({ addedNodes, removedNodes }) => {
+  //     const nodes = [...addedNodes, ...removedNodes]
+  //     return nodes.every(node => headNodeLastMutatedStylesheetNodes.includes(node))
+  //   })
+  //   if (onlyStylesheetsMutated) return;
+  //   const lmPageStylesheets = [...document.querySelectorAll('.lm-page-stylesheet')]
+  //   headNodeLastMutatedStylesheetNodes = [...lmPageStylesheets]
+  //   lmPageStylesheets.forEach(stylesheet => headNode.appendChild(stylesheet))
+  // })
+  // headObserver.observe(headNode, { childList: true })
+  
+  // [WIP] find something better after Adaptation
+  const lmPageStylesheets = [...document.querySelectorAll('link[href^="https://assets-decodeurs.lemonde.fr/redacweb"]')]
+  document.body.append(...lmPageStylesheets)
   
   // Load styles (dont await)
   document.head.innerHTML += `<link rel="stylesheet" href="${STYLES_INDEX_URL.toString()}">`
