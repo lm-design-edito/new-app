d02b4cc9294c2864d3addfe4e9bcc47be42d6c5b
Maxime Fabas <fabas@lemonde.fr>
2023-06-16 16:43:57 +0200

    ~/utils/smooth-scroll-to - uses element.scrollIntoView

--- src/utils/smooth-scroll-to/index.ts
+++ src/utils/smooth-scroll-to/index.ts
@@ -1,8 +1,3 @@
 export default function smoothScrollTo (element: Element) {
-  const elementBoundingClientRect = element.getBoundingClientRect()
-  const elementTop = elementBoundingClientRect.top
-  window.scrollTo({
-    top: elementTop,
-    behavior: 'smooth'
-  })
+  element.scrollIntoView({ behavior: 'smooth' })
 }
