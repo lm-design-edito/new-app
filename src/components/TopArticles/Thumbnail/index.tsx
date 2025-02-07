b162acdf192f14367290569579a77a35e57f3092
Maxime Fabas <fabas@lemonde.fr>
2025-02-07 15:22:52 +0100

    ~/components/TopArticles - perform a real fetch to forecast API again

--- src/components/TopArticles/Thumbnail/index.tsx
+++ src/components/TopArticles/Thumbnail/index.tsx
@@ -30,73 +30,79 @@ export default class Thumb extends Component<Props> {
       className="lm-thumbnail"
       href={targetUrl}
       onClick={onClick}>
-      <div className="lm-thumbnail__above">{contentAbove}</div>
-      <div className="lm-thumbnail__left">{contentLeft}</div>
+      <div className="lm-thumbnail__content lm-thumbnail__content--above">{contentAbove}</div>
+      <div className="lm-thumbnail__content lm-thumbnail__content--left">{contentLeft}</div>
       <div className="lm-thumbnail__image-wrapper">
         <img className="lm-thumbnail__image" src={imageSrc} alt={imageAlt} />
         <div className="lm-thumbnail__opacifier"></div>
-        <div className="lm-thumbnail__overlay">{contentOverlay}</div>
+        <div className="lm-thumbnail__content lm-thumbnail__content--overlay">{contentOverlay}</div>
       </div>
-      <div className="lm-thumbnail__right">{contentRight}</div>
-      <div className="lm-thumbnail__below">{contentBelow}</div>
-      <style>{`
-        a.lm-thumbnail {
-          display: grid;
-          width: fit-content;
-          grid-template-areas: 
-            "topleft above topright"
-            "left image right"
-            "botleft below botright";
-          grid-template-columns: auto auto auto;
-          grid-template-rows: auto auto auto;
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .lm-thumbnail__above {
-          grid-area: above;
-        }
-
-        .lm-thumbnail__left {
-          grid-area: left;
-          display: flex;
-          justify-content: flex-end;
-        }
-
-        .lm-thumbnail__right {
-          grid-area: right;
-          display: flex;
-        }
-
-        .lm-thumbnail__below {
-          grid-area: below;
-        }
-
-        .lm-thumbnail__image-wrapper {
-          position: relative;
-          width: fit-content;
-          grid-area: image;
-        }
-
-        .lm-thumbnail__image {
-          display: block;
-        }
-
-        .lm-thumbnail__opacifier {
-          position: absolute;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          pointer-events: none;
-          user-select: none;
-        }
-
-        .lm-thumbnail__overlay {
-          position: absolute;
-          top: 0;
-        }
-    `}</style>
+      <div className="lm-thumbnail__content lm-thumbnail__content--right">{contentRight}</div>
+      <div className="lm-thumbnail__content lm-thumbnail__content--below">{contentBelow}</div>
     </a>
   }
 }
+
+
+/*
+This CSS is assumed to be provided by the consumer app for now:
+
+a.lm-thumbnail {
+  display: grid;
+  width: fit-content;
+  grid-template-areas: 
+    "topleft above topright"
+    "left image right"
+    "botleft below botright";
+  grid-template-columns: auto auto auto;
+  grid-template-rows: auto auto auto;
+  color: inherit;
+  text-decoration: none;
+}
+
+lm-thumbnail__content lm-thumbnail__content--above {
+  grid-area: above;
+}
+
+lm-thumbnail__content lm-thumbnail__content--left {
+  grid-area: left;
+  display: flex;
+  justify-content: flex-end;
+}
+
+lm-thumbnail__content lm-thumbnail__content--right {
+  grid-area: right;
+  display: flex;
+}
+
+lm-thumbnail__content lm-thumbnail__content--below {
+  grid-area: below;
+}
+
+.lm-thumbnail__image-wrapper {
+  position: relative;
+  width: fit-content;
+  grid-area: image;
+}
+
+.lm-thumbnail__image {
+  display: block;
+}
+
+.lm-thumbnail__opacifier {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  pointer-events: none;
+  user-select: none;
+}
+
+lm-thumbnail__content lm-thumbnail__content--overlay {
+  position: absolute;
+  top: 0;
+}
+
+
+*/
