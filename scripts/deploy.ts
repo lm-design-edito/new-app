f80163d789560e02acf7e8dd66e16d10a0ef3a9b
Maxime Fabas <fabas@lemonde.fr>
2025-04-17 15:55:38 +0200

    /scripts/deploy - removed the npm publication step

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -368,7 +368,7 @@ async function buildSourceForLib () {
 /* * * * * * * * * * * * * * * * * * * * *
  * NPM publish lib
  * * * * * * * * * * * * * * * * * * * * */
-// async function npmPublishLib () {
+async function npmPublishLib () {
   console.log(styles.title(`Publishing lib to npm`))
   try {
     console.log(styles.regular(`Current npm registry lookup...\n`))
